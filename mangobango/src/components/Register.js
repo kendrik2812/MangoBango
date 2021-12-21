efaa78a7dfa55fa431b3179eeb9dafe9105d62bf
Jackcheal Dang <jackchealdang@gmail.com>
2021-12-20 23:34:18 -0600

    updated Register.js example counter

--- mangobango/src/components/Register.js
+++ mangobango/src/components/Register.js
@@ -1,11 +1,14 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default  Register{
-  render(){
-    return{
-      <div>
-        <h3>React SignUp Component</h3>
-      </div>
-    }
-  }
-} 
+function Register() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h3>React SignUp Component</h3>
+      <p>You clicked {count} time</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default Register;
